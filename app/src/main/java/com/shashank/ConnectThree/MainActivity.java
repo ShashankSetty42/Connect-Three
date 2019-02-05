85d91207d608dae7590872a8129ce4f3e4f6477b
Shashank Setty <shashanksetty.u@gmail.com>
2019-02-05 20:43:49 +0530

    test commit push

--- app/src/main/java/com/shashank/ConnectThree/MainActivity.java
+++ app/src/main/java/com/shashank/ConnectThree/MainActivity.java
@@ -317,8 +317,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         new AlertDialog.Builder(this)
-                .setTitle("Closing Activity")
-                .setMessage("Are you sure you want to close this activity?")
+                .setTitle("Exit")
+                .setMessage("Are you sure you want to exit?")
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener()
                 {
                     @Override
