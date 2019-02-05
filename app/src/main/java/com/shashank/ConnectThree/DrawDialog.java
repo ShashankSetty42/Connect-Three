1d4680e73ebf414677a2345d06a2cfb949d68998
Shashank Setty <shashanksetty.u@gmail.com>
2019-02-05 18:55:10 +0530

    Added custom Dialog Boxes

---
+++ app/src/main/java/com/shashank/ConnectThree/DrawDialog.java
@@ -0,0 +1,38 @@
+package com.shashank.ConnectThree;
+
+import android.app.Dialog;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+public class DrawDialog extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.draw, null);
+        builder.setView(view);
+        final AlertDialog drawDialog =  builder.create();
+        drawDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        Button noBtn = (Button) view.findViewById(R.id.no);
+        noBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        Button yesBtn = (Button) view.findViewById(R.id.yes);
+        yesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)getActivity()).resetBoard();
+                dismiss();
+            }
+        });
+        return drawDialog;
+    }
+}
